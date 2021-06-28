adabcdc8e7272163cd0563c991d98d87a3234c59
kkt9102 <kkt9102@gmail.com>
2021-06-29 00:11:17 +0900

    ArticleMapper.xml 앨리먼트 대문자 사용금지...

--- src/main/java/com/crud/controller/ArticleController.java
+++ src/main/java/com/crud/controller/ArticleController.java
@@ -1,17 +1,21 @@
 package com.crud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// 컨트롤러 설정
+// 而⑦듃濡ㅻ윭 �꽕�젙
 @Controller
-// 공툥 요청 경로
-@RequestMapping("/article/")
+// 怨듯닪 �슂泥� 寃쎈줈
+@RequestMapping("/article/*")
 public class ArticleController {
 	
+	private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
+	
 	@GetMapping("/list")
-	public String showList() {
-		return "article/list";
+	public void showList() {
+		return;
 	}
 }
